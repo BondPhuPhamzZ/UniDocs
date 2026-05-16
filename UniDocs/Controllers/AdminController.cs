dfad6afaf576190748f6cb3faa1f8bd471628c1b
BondSensei <phuphamwork89@gmail.com>
2026-05-16 22:42:10 +0700

    update thêm giao diện demo cho Admin

---
+++ UniDocs/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniDocs.Controllers
+{
+    // Admin
+    public class AdminController : Controller
+    {
+        // Quản lý môn học
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        // Quản lý người dùng (trạng thái tài khoản)
+        public IActionResult Users()
+        {
+            return View();
+        }
+        // Tài liệu bị báo cáo
+        public IActionResult Courses()
+        {
+            return View();
+        }
+    }
+}
