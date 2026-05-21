7b6999a442743e6b1b2af71b76ea45c994a66ae1
BondSensei <phuphamwork89@gmail.com>
2026-05-21 22:36:54 +0700

    Truyền db vào trang Danh mục môn học (Index của CourseController)

--- UniDocs/Controllers/CourseController.cs
+++ UniDocs/Controllers/CourseController.cs
@@ -1,14 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net.WebSockets;
+using UniDocs.Data;
 
 namespace UniDocs.Controllers
 {
     // ===== Quản lý môn học =====
     public class CourseController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public CourseController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         // Trang danh sách các môn học
         public IActionResult Index()
         {
-            return View();
+            var courses = _context.Courses.Include(c => c.Documents).ToList();
+            return View(courses);
         }
 
 
