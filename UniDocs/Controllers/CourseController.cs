ca09faabd2fce4cc24db4d30e2c13a5141d78042
BondSensei <phuphamwork89@gmail.com>
2026-05-22 23:47:00 +0700

    Thêm chức năng phân trang, đổ Database thành công lên trang Index và Detail của CourseController

--- UniDocs/Controllers/CourseController.cs
+++ UniDocs/Controllers/CourseController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using System.Net.WebSockets;
 using UniDocs.Data;
 
@@ -24,19 +25,39 @@ namespace UniDocs.Controllers
 
 
         // Trang chi tiết tài liệu của 1 môn (major.html)
-        public IActionResult Detail(int id)
+        public IActionResult Detail(int id, int page = 1)
         {
-            var course = _context.Courses
-                .Include(c => c.Documents.Where(d => d.IsApproved == true))
-                .ThenInclude(d => d.User)
-                .FirstOrDefault(c => c.Id == id);
+            int pageSize = 5; 
 
+            // Lấy thông tin môn học -> Hiển thị ra màn hình
+            var course = _context.Courses.FirstOrDefault(c => c.Id == id);
             if (course == null)
             {
                 return NotFound("Không tìm thấy môn học!");
             }
 
-            return View(course);
+            // Lưu thông tin môn học vào Viewbag
+            ViewBag.CourseName = course.CourseName;
+            ViewBag.Department = course.Department;
+            ViewBag.CourseId = course.Id;
+
+            // Tính toán tổng số trang
+            int totalDocs = _context.Documents.Count(d => d.CourseId == id && d.IsApproved == true);
+            ViewBag.TotalDocuments = totalDocs;
+            ViewBag.TotalPages = (int)Math.Ceiling((double)totalDocs / pageSize);
+            ViewBag.CurrentPage = page;
+
+            // Truy vấn bảng Documents (phân trang)
+            var documents = _context.Documents
+                .Include(d => d.User)
+                .Include(d => d.Course) // Partial-View in ra tên môn
+                .Where(d => d.CourseId == id && d.IsApproved == true)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            return View(documents);
+
         }
 
 
