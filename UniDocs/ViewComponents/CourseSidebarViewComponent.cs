89b9e0e74c04d21dc9b9ce8387ea84cd4ef90313
BondSensei <phuphamwork89@gmail.com>
2026-06-05 13:27:20 +0700

    Cập nhật lại CSS cho UI (xóa các modal demo ban đầu), những comment dư thừa

--- UniDocs/ViewComponents/CourseSidebarViewComponent.cs
+++ UniDocs/ViewComponents/CourseSidebarViewComponent.cs
@@ -4,25 +4,17 @@ using UniDocs.Data;
 
 namespace UniDocs.ViewComponents
 {
-    /*public class CourseSidebarViewComponent
-    {
-    }*/
-
-    // Kế thừa ViewComponent
     public class CourseSidebarViewComponent : ViewComponent
     {
         private readonly AppDbContext _context;
 
-        // Inject db
         public CourseSidebarViewComponent(AppDbContext context)
         {
             _context = context;
         }
 
-        // Side bar view dc gọi
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            // Lấy dsach môn học, kèm theo đếm số lượng tài liệu đã dc duyệt của môn đó
             var courses = await _context.Courses
                 .Include(c => c.Documents.Where(d => d.IsApproved == true))
                 .ToListAsync();
