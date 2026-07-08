831acfe8e42ea20e4c6b6aef31057fde2c576e26
BondSensei <phuphamwork89@gmail.com>
2026-07-08 13:34:28 +0700

    Refactor Pagination into shared PartialView (ViewModel + implementation for Course and Admin Courses)

---
+++ UniDocs/ViewModels/PaginationViewModel.cs
@@ -0,0 +1,9 @@
+namespace UniDocs.ViewModels
+{
+    public class PaginationViewModel
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public string UrlTemplate { get; set; }
+    }
+}
